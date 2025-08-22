82dd45d115090c31212e9daaea41ff7cd960ae5e
Anderson Reis <andersonpensephp@hotmai.com>
2025-08-22 00:42:26 -0300

    fix: route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <ErrorBoundary>
       <SearchContextProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/desafio-spotify">
           <QueryClientProvider client={queryClient}>
             <AppRoutes />
           </QueryClientProvider>
