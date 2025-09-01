220ec31183cf6c7f9415ee1ec16324ce545c0b22
Anderson Reis <andersonpensephp@hotmai.com>
2025-09-01 15:19:52 -0300

    fix: tratamento de refresh token

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -12,6 +12,22 @@ import type {
 
 const apiBaseUrl = import.meta.env.VITE_SPOTIFY_API;
 const limitPerPage = import.meta.env.VITE_LIMIT_PER_PAGE;
+const AUTH_URL = import.meta.env.VITE_SPOTIFY_AUTHORIZED;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+
+let isRefreshing = false;
+let failedQueue: { resolve: (token: string) => void; reject: (error: unknown) => void }[] = [];
+
+const processQueue = (error: unknown, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (token) {
+      prom.resolve(token);
+    } else {
+      prom.reject(error);
+    }
+  });
+  failedQueue = [];
+};
 
 const api = axios.create({
   baseURL: apiBaseUrl,
@@ -27,13 +43,72 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }).then((token) => {
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+          return api(originalRequest);
+        });
+      }
+    }
+    const refreshToken = localStorage.getItem('spotify_refresh');
+    isRefreshing = true;
+
+    if (!refreshToken) {
+      isRefreshing = false;
       localStorage.removeItem('access_token');
       localStorage.removeItem('spotify_refresh');
-
       window.location.href = '/';
+
+      return Promise.reject(error);
     }
+
+    try {
+      const body = new URLSearchParams({
+        client_id: CLIENT_ID,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+      });
+
+      const { data } = await axios.post(AUTH_URL + '/api/token', body, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+      });
+
+      if (!data) {
+        throw new Error(data.error.message || 'Nenhum token encontrado');
+      }
+
+      const newAccessToken = data.access_token;
+
+      localStorage.setItem('access_token', newAccessToken);
+
+      processQueue(null, newAccessToken);
+
+      originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+      return api(originalRequest);
+
+    } catch {
+      processQueue(error, null);
+
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('spotify_refresh');
+      window.location.href = '/';
+
+    } finally {
+      isRefreshing = false;
+    }
+
     return Promise.reject(error);
   }
 );
